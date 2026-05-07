e9554e2a86de9abbf5d9d8a95c628a28ffa53726
Tetrafocal <281458187+tetrafocal@users.noreply.github.com>
2026-05-06 23:30:39 -0700

    Add page mechanic, basic download page

--- src/utils/getFolderAndFileName.tsx
+++ src/utils/getFolderAndFileName.tsx
@@ -1,3 +1,12 @@
+export function getBasePathAndFileName(path: string): [string, string] {
+  const splitIndex = Math.max(path.lastIndexOf("/"), path.lastIndexOf("\\"));
+  if (splitIndex === -1) {
+    return ["", path];
+  } else {
+    return [path.substring(0, splitIndex), path.substring(splitIndex + 1)];
+  }
+}
+
 export function getFolderAndFileName(path: string): [string, string] {
   const split = path.split(/[/\\]/);
   if (split.length < 2) {
